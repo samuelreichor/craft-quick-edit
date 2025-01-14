d65b0f18a73669b9044beae8415d21a3d59bf842
samuelreichor <samuelreichor@gmail.com>
2025-01-14 18:12:06 +0100

    I am Batman

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for craft-quick-edit
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace samuelreichor\quickedit\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * craft-quick-edit settings
+ */
+class Settings extends Model
+{
+}
